afb454353526bb46569ca65d47e61e75c13ad3fa
R0n316 <manyakinsasha@gmail.com>
2024-07-30 22:14:27 +0300

    make a basic page with book search results

--- scripts/swiper-slider.js
+++
@@ -1,8 +0,0 @@
-const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 10,
-    freeMode: true,
-    navigation: false,
-    pagination: false,
-    freeModeSticky: true,
-    freeModeMomentum: false
-});
